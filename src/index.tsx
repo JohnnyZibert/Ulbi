92589df2ace34421e0674567b69c07c5dfeb31d5
zgirdan <zgirdan@mail.ru>
2023-02-28 10:31:37 +0300

    add login modal

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import { ThemeProvider } from './app/providers/ThemeProvider';
 import 'app/styles/index.scss';
+import { StoreProvider } from './app/providers/StoreProvider';
 
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundery';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
